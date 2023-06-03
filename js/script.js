17e7e2ed1757c2473ade28a7dca209e86e78ebd9
andthatsthetruth <126412891+andthatsthetruth@users.noreply.github.com>
2023-06-02 20:46:12 -0700

    modal menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-    $('.burger').on('click', function(){
-        $('.mob-nav').toggle();
-    });
+$(document).ready(function () {
+  $(".burger").on("click", function () {
+    $(".mob-nav").removeClass("hidden");
+    $(".overlay").removeClass("hidden");
+  });
 });
+
+$(document).ready(function () {
+  $(".overlay").on("click", function () {
+    $(".mob-nav").addClass("hidden");
+    $(".overlay").addClass("hidden");
+  });
+});
+
+// const overlay = document.querySelector(".overlay");
+// const modal = document.querySelector(".burger");
+
+// const closeModal = function () {
+//   overlay.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
+
+// overlay.addEventListener("click", closeModal);
